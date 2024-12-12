5d5875e0deddc73a2600b36c354eaac771dcd26a
unknown <facundo.pineyrua@anima.edu.uy>
2024-12-11 21:41:40 -0300

    codigo final

--- index.js
+++ index.js
@@ -2,32 +2,32 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("rese");
+  res.send("Reseñas");
 });
 const reviews = [
   {
     pelicula: 'Ace Ventura',
-    opinion: 'De las mejores peliculas de comedia que he visto en mi vida, ademas como actua Jim Carrey',
+    opinion: 'De las mejores peliculas de comedia que he visto en mi vida, ademas como actua Jim Carrey es lo que le da el humor a la pelicula',
     puntuacion: '10/10',
     meGusta: 148,
     noMeGusta: 58,
-    usuario: 'cricketbat',
+    usuario: 'Elpepe123',
   },
   {
-    pelicula: 'The Lion King',
-    opinion: 'A classic masterpiece with timeless music and story.',
+    pelicula: 'El rey leon',
+    opinion: 'Una ridiculez es esta pelicula, prefiero mil veces más la sirenita',
+    puntuacion: '2/10',
+    meGusta: 13,
+    noMeGusta: 256,
+    usuario: 'Jaimito-chan01',
+  },
+  {
+    pelicula: 'Replicas',
+    opinion: 'increible pelicula, la recomiendo totalmente, la actuación de Keanu Reeves es espectacular, como se mete en el pesronaje en cada escena',
     puntuacion: '10/10',
-    meGusta: 250,
-    noMeGusta: 12,
-    usuario: 'movielover92',
-  },
-  {
-    pelicula: 'Inception',
-    opinion: 'Mind-bending and thought-provoking, a must-watch.',
-    puntuacion: '9/10',
     meGusta: 320,
     noMeGusta: 20,
-    usuario: 'dreamcatcher',
+    usuario: 'CarlitosInsano47',
   },
 ];
 
