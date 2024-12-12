b000513eb7e44ab840a3f43988c8b4cbad316de6
unknown <facundo.pineyrua@anima.edu.uy>
2024-12-12 18:02:48 -0300

    ultimo avance

--- index.js
+++ index.js
@@ -2,33 +2,37 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Reseñas");
+  res.send("En esta página se podran ver algunas reseñas de algunas peliculas, por ejemplo El rey león, ect.. Para ver las reseñas nomas tienen que poner en la barra de busqueda al lado del puerto 3000, tendrán que poner el nombre de la pelicula de la cual quieran ver la reseña. Las reseñas disponibles para ver son: El rey leon, Ace Ventura y Replicas, el nombre de las peliculas a la hora de buscar su reseña debe estar todo junto, por ejemplo: localhost:3000/AceVentura, y ahí le dan enter y les debería de aparecer la reseña");
 });
-const reviews = [
-  {
+app.get('/AceVentura', (req, res) => {
+  res.send({
     pelicula: 'Ace Ventura',
     opinion: 'De las mejores peliculas de comedia que he visto en mi vida, ademas como actua Jim Carrey es lo que le da el humor a la pelicula',
     puntuacion: '10/10',
     meGusta: 148,
     noMeGusta: 58,
     usuario: 'Elpepe123',
-  },
-  {
-    pelicula: 'El rey leon',
-    opinion: 'Una ridiculez es esta pelicula, prefiero mil veces más la sirenita',
-    puntuacion: '2/10',
-    meGusta: 13,
-    noMeGusta: 256,
-    usuario: 'Jaimito-chan01',
-  },
-  {
-    pelicula: 'Replicas',
-    opinion: 'increible pelicula, la recomiendo totalmente, la actuación de Keanu Reeves es espectacular, como se mete en el pesronaje en cada escena',
-    puntuacion: '10/10',
-    meGusta: 320,
-    noMeGusta: 20,
-    usuario: 'CarlitosInsano47',
-  },
-];
-
+  },);
+},);
+  
+  app.get('/Elreyleon', (req, res) => {
+    res.send({pelicula: 'El rey leon',
+      opinion: 'Una ridiculez es esta pelicula, prefiero mil veces más la sirenita',
+      puntuacion: '2/10',
+      meGusta: 13,
+      noMeGusta: 256,
+      usuario: 'Jaimito-chan01',
+      
+    },);
+  });
+  app.get('/Replicas', (req, res) => {
+    res.send({ pelicula: 'Replicas',
+      opinion: 'increible pelicula, la recomiendo totalmente, la actuación de Keanu Reeves es espectacular, como se mete en el pesronaje en cada escena',
+      puntuacion: '10/10',
+      meGusta: 320,
+      noMeGusta: 20,
+      usuario: 'CarlitosInsano47',
+    },);
+  });
+ 
 app.listen(3000);
